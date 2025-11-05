43c76826c99445b122823ee7d79b81e0801a033a
Alexandre Sanson <alexandresanson8@gmail.com>
2025-11-05 17:37:50 -0300

    Image Understanding

--- llm.py
+++ llm.py
@@ -43,24 +43,24 @@ class UserTransactions(BaseModel):
 def msg_processing(msg: str):
 
     #prompt = "Extract the monetary value specified in the following message and convert to cents, returning 0 if no value specified. Also, include a short description, returning an empty string if nothing is provided: "
-    prompt = '''Você é um chatbot assistente financeiro.O usuário enviou uma mensagem descrevendo uma transação. Analize a mensagem e forneça:
+    prompt = '''Você é um chatbot assistente financeiro.O usuário enviou uma mensagem descrevendo uma transação. Analise a mensagem e forneça:
     1)O valor descrito na transação, em centavos, retornando 0 se nenhum valor foi descrito.
     2)A categoria da transação, retornando "Outros" caso não se encaixe em nenhuma alternativa'''
     prompt = prompt + msg
     response = client.models.generate_content(
     model="gemini-2.5-flash",
     contents=prompt,
-    config={
-        "response_mime_type": "application/json",
-        "response_schema": UserTransactions,
-    },
-    )   
+    config=types.GenerateContentConfig(
+            response_mime_type="application/json",
+            response_schema=UserTransactions,
+        ),
+    )
     print(response.parsed)
     print("Output: " + response.text)
     return response.text
 
 def voice_processing(audio_path):
-    prompt = '''Você é um chatbot assistente financeiro.O usuário enviou uma mensagem descrevendo uma transação. Analize a mensagem e forneça:
+    prompt = '''Você é um chatbot assistente financeiro.O usuário enviou uma mensagem descrevendo uma transação. Analise a mensagem e forneça:
     1)O valor descrito na transação, em centavos, retornando 0 se nenhum valor foi descrito.
     2)A categoria da transação, retornando "Outros" caso não se encaixe em nenhuma alternativa'''
 
@@ -89,10 +89,51 @@ def voice_processing(audio_path):
                 mime_type='audio/ogg',
             ),
         ],
+        config=types.GenerateContentConfig(
+            response_mime_type="application/json",
+            response_schema=UserTransactions,
+        ),
+    )
+
+    return response.text
+
+def photo_processing(photo_path):
+    prompt = '''Você é um chatbot assistente financeiro. O usuário enviou uma imagem descrevendo uma transação. Analise a mensagem e forneça:
+    1)O valor total descrito na transação, em centavos, retornando 0 se nenhum valor foi descrito.
+    Se a imagem descrever múltiplos itens, como por exemplo uma nota fiscal de supermercado, retorne o valor total.
+    Se forem listados descontos, subtraia do valor total
+    2)A categoria da transação, retornando "Outros" caso não se encaixe em nenhuma alternativa'''
+
+    '''
+    my_file = client.files.upload(file=audio_path)
+    response = client.models.generate_content(
+        model='gemini-2.5-flash',
+        contents=[
+            prompt,
+            my_file,
+        ],
         config={
         "response_mime_type": "application/json",
         "response_schema": UserTransactions,
-        },
+    },
+    )
+    '''
+    with open(photo_path, 'rb') as f:
+        photo_bytes = f.read()
+    response = client.models.generate_content(
+        model='gemini-2.5-flash',
+        contents=[
+            prompt,
+            types.Part.from_bytes(
+                data=photo_bytes,
+                mime_type='image/jpeg',
+            ),
+        ],
+        config=types.GenerateContentConfig(
+            response_mime_type="application/json",
+            response_schema=UserTransactions,
+        ),
+
     )
 
     return response.text
--- main.py
+++ main.py
@@ -2,13 +2,13 @@
 from dotenv import load_dotenv
 import logging
 import os
-import telegram
+#import telegram
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler, CallbackQueryHandler, filters
 import llm
-import dbmanager
+#import dbmanager as dbm
 import json
-from datetime import datetime, timezone
+#from datetime import datetime, timezone
 
 def load_environment_variables():
     try:
@@ -134,7 +134,7 @@ async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     match query.data:
         case "Confirmar":
             response = context.user_data["transaction"]
-            dbmanager.register_transaction(response)
+            #dbm.register_transaction(response)
             await query.edit_message_text(text="Transação Confirmada")
 
         case "Editar":
@@ -177,12 +177,16 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text=start_msg)
 
 # Responses
+async def registration(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user_id = update.message.chat.id
+    user_name = update.message.chat.effective_name
+    #dbm.register_user(user_id, user_name)
 
 
 async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user_id = update.message.chat.id
     user_name = update.message.chat.effective_name
-    dbmanager.register_user(user_id, user_name)
+    #dbmanager.register_user(user_id, user_name)
 
     
     message_type: str =update.message.chat.type
@@ -194,12 +198,16 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     print(date)
     context.user_data["transaction"] = response
     context.user_data["new_value"] = 0
-    reply = f'''Valor:R${response["value"]/100:.2f}\nCategoria:{response["category"]}'''
-    await update.message.reply_text(reply, reply_markup=reply_markup)
+    reply = f'''Valor: R${response["value"]/100:.2f}\nCategoria:{response["category"]}'''
+    #await update.message.reply_text(reply, reply_markup=reply_markup)
+    await update.message.reply_text(reply)
 
 
 async def handle_voice(update: Update, context: ContextTypes.DEFAULT_TYPE):
     ogg_path=None
+    user_id = update.message.chat.id
+    user_name = update.message.chat.effective_name
+    #dbm.register_user(user_id, user_name)
     try:
         voice: Voice = update.message.voice
         chat_id = update.effective_chat.id
@@ -212,7 +220,15 @@ async def handle_voice(update: Update, context: ContextTypes.DEFAULT_TYPE):
         if not os.path.exists(ogg_path):
             raise FileNotFoundError(f"Arquivo não encontrado após download: {ogg_path}")
         response = llm.voice_processing(ogg_path)
-        await update.message.reply_text(response)
+        response = json.loads(response)
+        response["ID"] = user_id
+        date = update.message.date
+        print(date)
+        context.user_data["transaction"] = response
+        context.user_data["new_value"] = 0
+        reply = f'''Valor: R${response["value"] / 100:.2f}\nCategoria:{response["category"]}'''
+        # await update.message.reply_text(reply, reply_markup=reply_markup)
+        await update.message.reply_text(reply)
 
     except Exception as e:
         print(f"Erro no processamento de voz: {e}")
@@ -226,9 +242,17 @@ async def handle_voice(update: Update, context: ContextTypes.DEFAULT_TYPE):
             except Exception as cleanup_error:
                 print(f"Erro ao remover arquivo temporário {ogg_path}: {cleanup_error}")
 
+async def handle_photo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user_id = update.message.chat.id
+    photo = update.message.photo[-1]
+    file = await photo.get_file()
 
-
-
+    # Download the photo
+    photo_path = f"data/{photo.file_id}"
+    await file.download_to_drive(photo_path)
+    #logger.info(f"Photo downloaded to {photo_path}")
+    response = llm.photo_processing(photo_path)
+    await update.message.reply_text(response)
 
 if __name__ == '__main__':
     print("Starting bot...")
@@ -239,11 +263,13 @@ if __name__ == '__main__':
     msg_handler = MessageHandler(filters.TEXT, handle_message)
     button_handler = CallbackQueryHandler(button)
     voice_handler = MessageHandler(filters.VOICE, handle_voice)
+    photo_handler = MessageHandler(filters.PHOTO, handle_photo)
 
     application.add_handler(start_handler)
     application.add_handler(msg_handler)
     application.add_handler(button_handler)
     application.add_handler(voice_handler)
+    application.add_handler(photo_handler)
 
     print("Polling...")
     application.run_polling(poll_interval=3)
